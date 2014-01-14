43a3897f58c3d04937cc75ff7f6e70b8c35e7944
Thomas GESELLE <raitomm@gmail.com>
2014-01-14 12:12:38 +0100

    Update code

--- classes/calcul.rb
+++ classes/calcul.rb
@@ -2,4 +2,17 @@ class Calcul < EventMachine::Connection
   def initialize
     super
   end
+
+  def receive_data data
+    Console.show "New calcul received : #{data.chomp}", 'info'
+    begin
+      result = eval data.chomp
+      send_data result
+      Console.show "Result : #{result}", 'info'
+    rescue NameError
+      send_data nil
+      Console.show "Can't calulcate !", 'error'
+    end
+    close_connection
+  end
 end
