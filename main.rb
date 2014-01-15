c7bfcd070242be06515e04de3047688b58c8b49e
Thomas GESELLE <raitomm@gmail.com>
2014-01-15 09:49:18 +0100

    Interface web + calcul

--- main.rb
+++ main.rb
@@ -1,15 +1,39 @@
-#Gestion des dépendances des gems
+#Inclusion des gems
 require 'colored'
 require 'eventmachine'
+require 'websocket-eventmachine-server'
 require 'logger'
 
-#Chargement automatique de toutes les clasess et helpers
+#Chargement automatique de tous les helpers
 Dir["./helpers/*.rb"].each {|file| require file }
-Dir["./classes/*.rb"].each {|file| require file }
 
 #Démarrage du serveur
 EM.run do
-  EM.start_server '0.0.0.0', 12369, Calcul
+  Console.show "Serveur démarré", 'success'
 
-  Console.show 'Server is running', 'success'
+  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 12369) do |ws|
+    #Fonction à l'ouverture du socket
+    ws.onopen do
+      Console.show "Client connecté", 'success'
+    end
+
+    #Calcul reçu
+    ws.onmessage do |msg|
+      Console.show "New calcul received : #{msg.chomp}", 'info'
+
+      begin
+        result = eval msg.chomp
+      rescue NameError, SyntaxError, ZeroDivisionError
+        result = nil
+      end
+
+      Console.show "Result : #{result}", 'info'
+      ws.send result
+    end
+
+    #Déconnexion du client
+    ws.onclose do
+      Console.show "Client deconnecté", 'info'
+    end
+  end
 end
